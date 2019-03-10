80493e9cf524a4b0233bcf7323803216f0d86fce
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-10 04:33:51 -0400

    Fixed up old methods.

--- USACO.java
+++ USACO.java
@@ -1,45 +1,47 @@
-import java.util.*;
+ import java.util.*;
 import java.io.*;
 
-private int col;
-private int row;
-private int ele;
-private int number;
-/*
-row = //first int in line.
-col = //second int in line.
-ele = //third int in line.
-number = //fourth int in line.
-*/
-
-
 public class USACO {
+
+  public int col;
+  public int row;
+  public int ele;
+  public int number;
+  /*
+  row = //first int in line.
+  col = //second int in line.
+  ele = //third int in line.
+  number = //fourth int in line.
+  */
    public static int bronze(String filename){
     File f = new File(filename);
     Scanner s = new Scanner(f);
-    for (int i = 0; i < 4; i++){
-      //reads through each line.
-      String result;
-      result += s.nextLine();
 
-/*
-  private void subtract(int r, int c) {
-    int count;
-    for (int i = 0; i < 2; i++){
-      if (data[r][c] >data[r + i][c]){
-        data[r][c] = count;
-      }
-      else {
-        data[r + i][c] = count;
-      }
-      if (data[r][c] > data[r][c + i]){
-        data[r][c] = count;
-      }
+    String[] info = scan.nextLine().split(" ");
+    row = Integer.parseInt(RCEN[0]);
+    col = Integer.parseInt(RCEN[1]);
+    ele = Integer.parseInt(RCEN[2]);
+    number = Integer.parseInt(RCEN[3]);
 
-    }
+      int[][] lake = new int[row][col];
 
-  }
-  */
+          for (int i = 0; i < row; i++){
+            for (int x = 0; x < col; x++){
+              lake[i][x] = scan.nextInt();
+            }
+          }
+
+    int[][] moves = new int[number][3];
+
+    for(int i = 0; i < number; i++){
+     for (int x = 0; x < 3; x++){
+       moves[i][i2] = scan.nextInt();
+       //System.out.println(moves[i][i2]);
+     }
+   }
+
+
+
 
   public static int silver(String filename) {
 
