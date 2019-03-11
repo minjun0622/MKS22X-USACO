d8ad1b04aafc97598a34a7ecfc7b93a059ced534
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-10 21:44:17 -0400

    Finished but I get 0 so not finished.

---
+++ Driver.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver{
+  public static void main(String[] commandLine) throws FileNotFoundException{
+    USACO testing = new USACO();
+    System.out.println(testing.bronze("makelake1.in"));
+
+  }
+}
--- USACO.java
+++ USACO.java
@@ -8,78 +8,78 @@ public class USACO {
   ele = //third int in line.
   number = //fourth int in line.
   */
-   public static int bronze(String filename){
+   public static int bronze(String filename)throws FileNotFoundException{
     File f = new File(filename);
     Scanner s = new Scanner(f);
 
     String[] info = s.nextLine().split(" ");
-    int row = Integer.parseInt(info[0]);
-    int col = Integer.parseInt(info[1]);
-    int ele = Integer.parseInt(info[2]);
-    int number = Integer.parseInt(info[3]);
 
-      int[][] lake = new int[row][col];
+    int r = Integer.parseInt(info[0]);
+    int c = Integer.parseInt(info[1]);
+    int e = Integer.parseInt(info[2]);
+    int n = Integer.parseInt(info[3]);
 
-          for (int i = 0; i < row; i++){
-            for (int x = 0; x < col; x++){
-              lake[i][x] = scan.nextInt();
-            }
-          }
+    int[][] lake = new int[r][c];
+
+    for (int i = 0; i < row; i++){
+      for (int x = 0; x < col; x++){
+        lake[i][x] = s.nextInt();
+      }
+    }
 
     int[][] moves = new int[number][3];
 
     for(int i = 0; i < number; i++){
      for (int x = 0; x < 3; x++){
-       moves[i][i2] = scan.nextInt();
-       //System.out.println(moves[i][x]);
+       moves[i][x] = s.nextInt();
+       System.out.println(moves[i][x]);
      }
    }
 
    for (int i = 0; i < moves.length; i++){
-    int row = moves[i][0] - 1;
-    int col = moves[i][1] - 1;
+    int row2 = moves[i][0] - 1;
+    int col2 = moves[i][1] - 1;
     int subtract = moves[i][2];
 
     int max = 0;
 
-    for (int i = row; i-row < 3 && i < r; i++){
-      for (int x = col; x-col < 3 && x < c; x++){
-        if (lake[i][x] > max){
-          max = lake[i][x];
+    for (int i2 = row2; i2- row2 < 3 && i2 < row; i2++){
+      for (int x2 = col2; x2 -col2 < 3 && x2 < col; x2++){
+        if (lake[i2][x2] > max){
+          max = lake[i2][x2];
         }
       }
     }
+
     max -= subtract;
 
-    for (int i = row; i-row < 3 && i < r; i++){
-      for (int x = col; x-col < 3 && x < c; x++){
-        if (lake[i][x] > max){
-          lake[i][x] = max;
+    for (int i3 = row2; i3 -row2 < 3 && i3 < row; i3++){
+      for (int x3 = col2; x3 -col2 < 3 && x3 < col; x3++){
+        if (lake[i3][x3] > max){
+          lake[i3][x3] = max;
         }
       }
     }
 
-
-}
-
-
-
-
-  public static int silver(String filename) {
-
+    for (int ii = 0; ii < lake.length; ii++){
+      for (int xx = 0; xx < lake[0].length; xx++){
+        System.out.print(lake[ii][xx] + " ");
+      }
+      System.out.print("\n");
+    }
   }
 
+  int totDepth = 0;
 
+  for (int i = 0; row < lake.length; i++){
+    for (int x = 0; col < lake[0].length; x++){
+      if (ele - lake[i][x] > 0){
+        totDepth += ele - lake[i][x];
+      }
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
+  return totDepth * 72 * 72;
+}
 
 }
